9ee90c23e9a5bf7db95504135173127dcfb61c06
Roya Lotfi <lotfir@myumanitoba.ca>
2020-11-06 17:17:02 -0600

    added comments for rendering an element dynamically

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
 class Counter extends Component {
+    //instead of hardcoding the hello world we want to render it dynamically: 
+    // so in the counter class we need to add a property called state and set it to an object 
+
 
     render() { 
         return (<React.Fragment><h1> Hello World</h1> <h2>how are you</h2></React.Fragment>);
