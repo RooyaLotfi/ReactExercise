e2165eb0430c922ea1bc7ee3611a47006672c1f2
Roya Lotfi <lotfir@myumanitoba.ca>
2020-11-06 17:58:01 -0600

    added count element and rendered it in span dynamically

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -4,10 +4,24 @@ import ReactDom from 'react-dom';
 class Counter extends Component {
     //instead of hardcoding the hello world we want to render it dynamically: 
     // so in the counter class we need to add a property called state and set it to an object 
+    // state is a special property in react component. It is any data that this component needs. for example
+    // here state has a property called count and we set it to zero
+    // so this state object includes any data this component needs
+    state = {
+        count:0
+    };
 
-
+    // in this span element we want to render something dynamically: between curly braces you can add any valid javascript expressions if //
+    //we want to render the value of count property. An expression is something that produces some values //
     render() { 
-        return (<React.Fragment><h1> Hello World</h1> <h2>how are you</h2></React.Fragment>);
+        return (
+        <React.Fragment>
+        <h1> Hello World</h1> 
+            
+        <span>{this.state.count}</span>
+        <h2>how are you</h2>
+        </React.Fragment>
+        );
     }
 }
 
