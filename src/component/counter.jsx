ab9ba5294d558edc7c19103dc95212e358650b4f
Roya Lotfi <lotfir@myumanitoba.ca>
2020-11-06 16:31:12 -0600

    added counter component

---
+++ src/component/counter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class Counter extends Component {
+
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Counter;
